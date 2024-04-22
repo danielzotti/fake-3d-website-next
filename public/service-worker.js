59ccef9ca6eac441f09665b87be97d3273447833
Daniel Zotti <daniel.zotti87@gmail.com>
2024-04-22 17:55:34 +0200

    Fix manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
     if (window !== undefined && ("serviceWorker" in navigator)) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register("/sw.js").then((registration) => {
+            navigator.serviceWorker.register("sw.js").then((registration) => {
                 console.debug("Service Worker registered successfully");
             });
         });
