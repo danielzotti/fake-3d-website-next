f29ec10edca2a6537921b7c9d52e4e18e8535ee0
Daniel Zotti <daniel.zotti87@gmail.com>
2024-04-22 17:44:20 +0200

    Fix manifest type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
             <meta charSet="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="icon" href="images/favicon.png"/>
-            <link rel="manifets" href="manifest.json"/>
+            <link rel="manifest" href="manifest.json"/>
         </head>
         <body>
         {children}
